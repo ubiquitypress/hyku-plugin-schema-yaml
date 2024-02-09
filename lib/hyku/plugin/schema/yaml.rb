3173955e3a720fce6ecc5e1f587956e8f01fb074
Martin Simpson <martin.simpson@ubiquitypress.com>
2024-02-09 15:18:12 +0700

    Require the presenter

--- lib/hyku/plugin/schema/yaml.rb
+++ lib/hyku/plugin/schema/yaml.rb
@@ -1,3 +1,7 @@
+#TODO: Find out why this is necessary
+require_relative '../../../../app/presenters/hyku/plugin/schema/yaml/presenter.rb'
+
+# These are fine...
 require "hyku/plugin/schema/yaml/engine"
 require "hyku/plugin/schema/yaml/hyrax/form_fields"
 require "hyku/plugin/schema/yaml/hyrax/indexer"
